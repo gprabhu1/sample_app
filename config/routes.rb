bf353e6208e2da0a72799f844a4237a1bc75c1bd
Ganapathi Prabhu <gprabhu1@umbc.edu>
2013-04-22 00:43:39 +0000

    Add user microposts with Exercises 2, 4, 5, 6

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ SampleApp::Application.routes.draw do
   resources :entries
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
-  root :to => 'entries#index'
+#  root :to => 'entries#index'
   root to: 'static_pages#home'  
 
   match '/signup',  to: 'users#new'
